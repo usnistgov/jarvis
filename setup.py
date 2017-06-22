1b9b958c671696879fa4b311b87790bef8230ff7
Kamal Choudhary <writetokamal.1989@gmail.com>
2017-06-22 15:49:30 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+import glob
+import os
+
+from setuptools import setup, find_packages
+
+JARVIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name="JARVIS",
+    version="1.0.0",
+    install_requires=["pymatgen>=4.7.0", "FireWorks>=1.4.0",
+                      "custodian>=1.0.1", "pymatgen-db>=0.5.1",
+                      "ase>=3.11.0", "six", "pyhull>=1.5.3"],
+    extras_require={"babel": ["openbabel", "pybel"],
+                    "remote": ["fabric"],
+                    "doc": ["sphinx>=1.3.1", "sphinx-rtd-theme>=0.1.8"]
+                    },
+    author="Kamal Choudhary",
+    author_email = "kamal.choudhary@nist.gov"
+    description=(
+        "High throughput computation with density functional theory, molecular dynamics and machine learning"),
+    license="MIT",
+    url="https://github.com/usnistgov/jarvis",
+    packages=find_packages(),
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",        
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    scripts=glob.glob(os.path.join(JARVIS_DIR,  "*"))
+)
