ac24af28827f0cfccb682f398d8ad14e099ebf9f
Kamal Choudhary <writetokamal.1989@gmail.com>
2017-10-09 22:16:14 -0400

    setup update 1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ JARVIS_DIR = os.path.dirname(os.path.abspath(__file__))
 setup(
     name="jarvis",
     version="1.0.0",
-    install_requires=["pymatgen==3.3.5","monty==0.9.6","scipy==0.17.0","phonopy==1.10.9","custodian>=1.0.4","ase==3.13.0","scikit-learn==0.18.1"],
+    install_requires=["pymatgen==3.3.5","monty==0.9.6","numpy==1.10.4","spglib==1.9.9.18","pyspglib==1.8.3.1","scipy==0.17.0","phonopy==1.10.9","custodian>=1.0.4","ase==3.13.0","scikit-learn==0.18.1"],
     extras_require={"babel": ["openbabel", "pybel"],
                     "remote": ["fabric"],
                     "doc": ["sphinx>=1.3.1", "sphinx-rtd-theme>=0.1.8"]
