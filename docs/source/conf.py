d203a4638666978286c22faeeb3699f4d37dbf12
JARVIS-Unifies <writetokamal.1989@gmail.com>
2018-10-22 15:02:30 -0400

    docs_ext

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,9 +38,8 @@ release = 'v2018.10.22'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-]
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', "sphinx.ext.mathjax"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
