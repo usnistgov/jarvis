2d32924d6d2110996dd2b53a06749337287f6f73
JARVIS-Unifies <writetokamal.1989@gmail.com>
2018-10-22 22:25:51 -0400

    env

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -2,8 +2,8 @@
 Integration of JARVIS-FF/DFT/ML with genetic algorithm.
 """
 import os
-env=str(os.path.join(os.path.dirname(__file__),'env_variables'))
-#env=str('env_variables'))
+#env=str(os.path.join(os.path.dirname(__file__),'env_variables'))
+env=str('env_variables'))
 f=open(env,'r')
 lines=f.read().splitlines()
 f.close()
