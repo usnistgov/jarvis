dc1bae0f79c1e04cecae1594ec967f6524237aba
JARVIS-Unifies <writetokamal.1989@gmail.com>
2018-10-22 22:36:25 -0400

    circ6

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -3,14 +3,18 @@ Integration of JARVIS-FF/DFT/ML with genetic algorithm.
 """
 import os
 #env=str(os.path.join(os.path.dirname(__file__),'env_variables'))
-dir = os.path.join(os.path.dirname(__file__))
-env=os.path.join(dir, 'env_variables')
-#env=str('env_variables'))
-f=open(env,'r')
-lines=f.read().splitlines()
-f.close()
-for i in lines:
-  tmp=(i.split('='))
-  if len(tmp)>1:
-   print (tmp[0],tmp[1])
-   os.environ[tmp[0]]=tmp[1]
+try:
+    dir = os.path.join(os.path.dirname(__file__))
+    env=os.path.join(dir, 'env_variables')
+    #env=str('env_variables'))
+    f=open(env,'r')
+    lines=f.read().splitlines()
+    f.close()
+    for i in lines:
+      tmp=(i.split('='))
+      if len(tmp)>1:
+       print (tmp[0],tmp[1])
+        os.environ[tmp[0]]=tmp[1]
+except:
+   print ('Cannot find env_variable in site-packages')
+   pass
