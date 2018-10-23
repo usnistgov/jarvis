de17db9e51cf8e2d598aabca46cb960801c64750
JARVIS-Unifies <writetokamal.1989@gmail.com>
2018-10-23 17:08:15 -0400

    install_req

--- setup.py
+++ setup.py
@@ -8,12 +8,10 @@ JARVIS_DIR = os.path.dirname(os.path.abspath(__file__))
 setup(
     name="jarvis-tools",
     version="2018.10.22.3",
-    setup_requires=['numpy==1.15.2','scipy==1.1.0'],
-    install_requires=["numpy==1.15.2","scipy==1.1.0",],
+    install_requires=["numpy==1.15.2","scipy==1.1.0","phonopy==1.11.2","pymatgen==2017.8.4","custodian==1.0.4","ase==3.11.0","pybtex==0.21","fortranformat==0.2.5","lightgbm==2.1.0","scikit-learn==0.18.1","pandas==0.23.4",],
 
     extras_require={"babel": ["openbabel", "pybel"],
                     "doc": ["sphinx>=1.3.1", "sphinx-rtd-theme>=0.1.8"],
-                    "pymatgen_related": ["pymatgen==2017.8.4", "custodian==1.0.4","phonopy==1.11.2","ase==3.11.0","pybtex==0.21","fortranformat==0.2.5","lightgbm==2.1.0","scikit-learn==0.18.1","pandas==0.23.4",]
                     },
     author="Kamal Choudhary",
     author_email = "kamal.choudhary@nist.gov",
