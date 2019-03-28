13342c49ebc70a6ea5c7b9359c4174a668cc6229
Daniel Wheeler <daniel.wheeler2@gmail.com>
2019-03-28 17:39:23 -0400

    Try with older version of nixpkgs

--- nix/nixpkgs_version.nix
+++ nix/nixpkgs_version.nix
@@ -3,8 +3,8 @@ let
   nixpkgs_download = fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs-channels";
-    rev = "7f35ed9df40f12a79a242e6ea79b8a472cf74d42";
-    sha256 = "1wr6dzy99rfx8s399zjjjcffppsbarxl2960wgb0xjzr7v65pikz";
+    rev = "6a3f5bcb061e1822f50e299f5616a0731636e4e7"; #"7f35ed9df40f12a79a242e6ea79b8a472cf74d42";
+    sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd"; #"1wr6dzy99rfx8s399zjjjcffppsbarxl2960wgb0xjzr7v65pikz";
   };
 in
   import nixpkgs_download {}
