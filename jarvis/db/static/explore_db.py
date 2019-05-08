600b90bea99a9739ce4d56bc0a78e7397f3f27ee
knc6 <kamal.choudhary@nist.gov>
2019-05-08 00:38:11 -0700

    explre db update

--- jarvis/db/static/explore_db.py
+++ jarvis/db/static/explore_db.py
@@ -1,42 +1,39 @@
 from monty.json import MontyEncoder, MontyDecoder
 from monty.serialization import loadfn, dumpfn
+import os,requests
 
-"""
- Download 3D material data from https://figshare.com/articles/jdft_3d-7-7-2018_json/6815699
- Download 2D material data from https://figshare.com/articles/jdft_2d-7-7-2018_json/6815705 
+def get_3d_dataset():
+ j3d = str(os.path.join(os.path.dirname(__file__), "j3d.json"))
+ if not os.path.isfile(j3d):
+    r = requests.get('https://ndownloader.figshare.com/files/12468083')
+    f=open(j3d,'wb')
+    f.write(r.content)
+    f.close()
+ data_3d = loadfn(j3d, cls=MontyDecoder)
+ return data_3d
 
- Website: https://jarvis.nist.gov
- https://www.nature.com/articles/s41598-017-05402-0
- https://www.nature.com/articles/sdata201882
- https://journals.aps.org/prb/abstract/10.1103/PhysRevB.98.014107
-"""
+def get_2d_dataset():
+ j2d = str(os.path.join(os.path.dirname(__file__), "j2d.json"))
+ if not os.path.isfile(j2d):
+    r = requests.get('https://ndownloader.figshare.com/files/12394556')
+    f=open(j2d,'wb')
+    f.write(r.content)
+ data_2d = loadfn(j2d, cls=MontyDecoder)
+ return data_2d
 
+def get_ml_dataset():
+ jml = str(os.path.join(os.path.dirname(__file__), "jml.json"))
+ if not os.path.isfile(jml):
+    r = requests.get('https://ndownloader.figshare.com/files/12533579')
+    f=open(jml,'wb')
+    f.write(r.content)
+ data_ml = loadfn(jml, cls=MontyDecoder)
+ return data_ml
 
 if __name__ == "__main__":
-    d = loadfn("jdft_3d.json", cls=MontyDecoder)
-    # d=loadfn('jdft_2d.json',cls=MontyDecoder)
-
-    print("Total size", len(d))
-    for i in d:
-        print(
-            "JARVIS id",
-            i["jid"],
-            "link",
-            str("https://www.ctcms.nist.gov/~knc6/jsmol/")
-            + str(i["jid"])
-            + str(".html"),
-        )
-        print("Materials project id"), i["mpid"], "link", str(
-            "https://www.materialsproject.org/materials/"
-        ) + str(i["mpid"])
-        print("final energy eV", i["fin_en"])
-        print("magnetic moment, Bohr Mag.", i["magmom"])
-        print("optb88vdw bandgap eV", i["op_gap"])
-        print("Tran-Blaha MBJ bandgap  eV", i["mbj_gap"])
-        print(
-            "static dielectric function in x,optb88vdw", i["epsx"]
-        )  # Careful about 2D
-        print("static dielectric function in x,MBJ", i["mepsx"])  # Careful about 2D
-        print("bulk modulus GPa", i["kv"])  # Careful about 2D
-        print("Shear modulus GPa", i["gv"])  # Careful about 2D
-        break
+     data_2d = get_2d_dataset()
+     print (len(data_2d))
+     data_3d = get_3d_dataset()
+     print (len(data_3d))
+     data_ml = get_ml_dataset()
+     print (len(data_ml))
