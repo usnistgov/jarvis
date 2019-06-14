504935e4af84659fbc7c55ea54c43f51f01021ca
knc6 <kamal.choudhary@nist.gov>
2019-06-14 10:33:14 -0700

    circleci fix

--- jarvis/tests/test_explore_db.py
+++ jarvis/tests/test_explore_db.py
@@ -2,10 +2,10 @@ from jarvis.db.static.explore_db import get_3d_dataset,get_2d_dataset,get_ml_dat
 
 def test_j2d():
     data_2d = get_2d_dataset()
-    assert (len(data_2d))==636
+    assert (len(data_2d))==754
 def test_j3d():
     data_3d = get_3d_dataset()
-    assert (len(data_3d))==25923
+    assert (len(data_3d))==32486
 def test_jml():
     data_ml = get_ml_dataset()
     assert (len(data_ml))==24759
