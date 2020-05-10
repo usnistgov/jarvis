9e508f067b7ad90fb46ab5e6b8272c0e6c6e26a9
JARVIS-Unifies <writetokamal.1989@gmail.com>
2020-05-10 04:46:34 -0400

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['*testfiles*']
-
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
