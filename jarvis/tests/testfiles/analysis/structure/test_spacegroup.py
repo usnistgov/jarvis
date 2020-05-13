4afbb78a24b9a7bfefa6cf77afb07c3a5980eeb3
JARVIS-Unifies <writetokamal.1989@gmail.com>
2020-05-13 01:43:46 -0400

    increased pytests

--- jarvis/tests/testfiles/analysis/structure/test_spacegroup.py
+++ jarvis/tests/testfiles/analysis/structure/test_spacegroup.py
@@ -4,7 +4,10 @@ from jarvis.analysis.structure.spacegroup import (
     get_wyckoff_position_operators,
 )
 from jarvis.core.atoms import Atoms
+from jarvis.io.vasp.inputs import Poscar
 
+s1 = Poscar.from_file(os.path.join(os.path.dirname(__file__), "..","defects","POSCAR-JVASP-667")).atoms
+s2 = Poscar.from_file(os.path.join(os.path.dirname(__file__), "..","..","io","wannier","POSCAR")).atoms
 
 def test_spg():
     box = [[2.715, 2.715, 0], [0, 2.715, 2.715], [2.715, 0, 2.715]]
@@ -24,6 +27,11 @@ def test_spg():
         ml[0][0],
         x["wyckoff"][0]["letter"],
     ) == (227, "Fd-3m", 8, 1, "l")
+    spg = Spacegroup3D(atoms=s1)
+    cvn = spg.conventional_standard_structure
+    spg = Spacegroup3D(atoms=s2)
+    cvn = spg.conventional_standard_structure
 
+   
 
 # test_spg()
