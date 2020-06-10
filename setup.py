a015649d335f2ba6260f502a54aad8818a55c9f9
JARVIS-Unifies <writetokamal.1989@gmail.com>
2020-06-10 02:06:51 -0400

    version number update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(base_dir, "README.rst")) as f:
 
 setup(
     name="jarvis-tools",
-    version="2020.6.8",
+    version="2020.6.9",
     long_description=long_d,
     install_requires=[
         "numpy",
