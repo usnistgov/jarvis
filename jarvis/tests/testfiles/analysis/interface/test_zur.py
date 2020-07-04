2e4f57838fb1d4bfe352b96badf153a0202afe6a
JARVIS-Unifies <writetokamal.1989@gmail.com>
2020-07-04 16:33:05 -0400

    Increased test coverage

--- jarvis/tests/testfiles/analysis/interface/test_zur.py
+++ jarvis/tests/testfiles/analysis/interface/test_zur.py
@@ -1,4 +1,10 @@
-from jarvis.analysis.interface.zur import ZSLGenerator,get_hetero_type, mismatch_strts, get_hetero,make_interface
+from jarvis.analysis.interface.zur import (
+    ZSLGenerator,
+    get_hetero_type,
+    mismatch_strts,
+    get_hetero,
+    make_interface,
+)
 from jarvis.core.atoms import Atoms
 from jarvis.io.vasp.inputs import Poscar
 import os
@@ -11,30 +17,31 @@ s4 = Poscar.from_file(os.path.join(os.path.dirname(__file__), "POSCAR-JVASP-7517
 
 def test_zur():
     info = mismatch_strts(film=s4.atoms, subs=s2.atoms)
-    a = info['film_sl'].center_around_origin()
-    b = info['subs_sl'].center_around_origin()
-    combined  = get_hetero(a,b,seperation=3).center_around_origin()
-    info = make_interface(film=s4.atoms,subs=s2.atoms)
-    combined = info['interface']
-    print (combined)
+    a = info["film_sl"].center_around_origin()
+    b = info["subs_sl"].center_around_origin()
+    combined = get_hetero(a, b, seperation=3).center_around_origin()
+    info = make_interface(film=s4.atoms, subs=s2.atoms)
+    combined = info["interface"]
+    print(combined)
     info = mismatch_strts(film=s2.atoms, subs=s3.atoms)
-    a = info['film_sl'].center_around_origin()
-    b = info['subs_sl'].center_around_origin()
-    combined  = get_hetero(a,b,seperation=3).center_around_origin()
-    info = make_interface(film=s2.atoms,subs=s3.atoms)
-    combined = info['interface']
-    print (combined)
+    a = info["film_sl"].center_around_origin()
+    b = info["subs_sl"].center_around_origin()
+    combined = get_hetero(a, b, seperation=3).center_around_origin()
+    info = make_interface(film=s2.atoms, subs=s3.atoms)
+    combined = info["interface"]
+    print(combined)
     info = mismatch_strts(film=s1.atoms, subs=s2.atoms)
-    a = info['film_sl'].center_around_origin()
-    b = info['subs_sl'].center_around_origin()
-    combined  = get_hetero(a,b,seperation=3).center_around_origin()
-    info = make_interface(film=s1.atoms,subs=s2.atoms)
-    combined = info['interface']
-    print (combined)
+    a = info["film_sl"].center_around_origin()
+    b = info["subs_sl"].center_around_origin()
+    combined = get_hetero(a, b, seperation=3).center_around_origin()
+    info = make_interface(film=s1.atoms, subs=s2.atoms)
+    combined = info["interface"]
+    print(combined)
     assert (round(info["mismatch_u"], 3), round(info["mismatch_angle"], 3)) == (
         0.002,
         0.0,
     )
+
 
 def test_type():
     B = {}
@@ -45,5 +52,5 @@ def test_type():
     A["scf_vbm"] = -7
     A["scf_cbm"] = -6
     A["avg_max"] = -2
-    int_type,stack = get_hetero_type(A=A,B=B)       
-    assert (int_type,stack)==('III','BA')
+    int_type, stack = get_hetero_type(A=A, B=B)
+    assert (int_type, stack) == ("III", "BA")
--- jarvis/tests/testfiles/tasks/test_vasp.py
+++ jarvis/tests/testfiles/tasks/test_vasp.py
@@ -1,20 +1,16 @@
 import os
 
 vasp_path = os.path.join(
-    os.path.dirname(__file__),
-    "..",
-    "..",
-    "..",
-    "examples",
-    "vasp",
-    "SiOptb88"
+    os.path.dirname(__file__), "..", "..", "..", "examples", "vasp", "SiOptb88"
 )
 
 cwd = str(os.getcwd())
 
+
 def test_vasp():
     os.chdir(vasp_path)
-    cmd = str('python master.py')
+    cmd = str("python master.py")
     os.system(cmd)
 
-os.chdir(cwd)    
+
+os.chdir(cwd)
