f1997980c090d3be3f44ed9735eaa94d4e550065
Kamal Choudhary <writetokamal.1989@gmail.com>
2020-07-08 17:24:45 -0400

    Increasing test coverage in LAMMPS modules (#80)
    * ZSL fix
    
    * Added pytests for spacegroup
    
    * Increasing pytest coverage
    
    * Increasing pytest coverage

--- jarvis/tests/testfiles/io/lammps/test_inputs.py
+++ jarvis/tests/testfiles/io/lammps/test_inputs.py
@@ -5,21 +5,39 @@ import os
 
 data = os.path.join(os.path.dirname(__file__), "lammps.data")
 init = os.path.join(os.path.dirname(__file__), "init.mod")
-pot = os.path.join(os.path.dirname(__file__), "potential.mod")
 inm = os.path.join(os.path.dirname(__file__), "in.main")
+pot =(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "..",
+        "..",
+        "..",
+        "examples",
+        "lammps",
+        "Al03.eam.alloy_nist",
+        "bulk@mp-134_fold",
+        "bulk@cellmax4",
+        "potential.mod",
+    )
+)
 
 
 def test_inputs():
     box = [[2.715, 2.715, 0], [0, 2.715, 2.715], [2.715, 0, 2.715]]
     coords = [[0, 0, 0], [0.25, 0.25, 0.25]]
-    elements = ["Si", "Si"]
-    Si = Atoms(lattice_mat=box, coords=coords, elements=elements)
-    spg = Spacegroup3D(atoms=Si)
-    Si = spg.conventional_standard_structure
-    box = LammpsData().atoms_to_lammps(atoms=Si)
-    box.write_file(data)
+    elements = ["Al", "Al"]
+    Al = Atoms(lattice_mat=box, coords=coords, elements=elements)
+    spg = Spacegroup3D(atoms=Al)
+    Al = spg.conventional_standard_structure
+    box = LammpsData().atoms_to_lammps(atoms=Al)
+    box.write_file(filename=data)
+    d = box.to_dict()
+    # print ('d=',d)
+    reload_d = LammpsData.from_dict(d)
+    # print ('d=',reload_d)
     atoms = box.lammps_to_atoms()
-    lmp = LammpsData().read_data(data)
+    lmp = LammpsData().read_data(filename=data, potential_file=pot)
     lmp = LammpsData().atoms_to_lammps(atoms=atoms)
     pair_coeff = "abc"
     inp = LammpsInput(LammpsDataObj=lmp).write_lammps_in(
@@ -35,10 +53,10 @@ def test_inputs():
     )
 
     assert (lmp._lammps_box[1], atoms.num_atoms) == (5.43, 8)
-    os.remove(data)
-    os.remove(init)
-    os.remove(inm)
-    os.remove(pot)
+    #os.remove(data)
+    #os.remove(init)
+    #os.remove(inm)
+    #os.remove(pot)
 
 
-test_inputs()
+# test_inputs()
