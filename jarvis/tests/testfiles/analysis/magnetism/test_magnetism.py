0b42ee1e03413c560642b7449c0bc3229dd8d7da
JARVIS-Unifies <writetokamal.1989@gmail.com>
2020-08-07 20:49:38 -0400

    Magnetic ordering,EFG setup, Interface fix

--- jarvis/tests/testfiles/analysis/magnetism/test_magnetism.py
+++ jarvis/tests/testfiles/analysis/magnetism/test_magnetism.py
@@ -1,22 +1,19 @@
 from jarvis.core.atoms import Atoms
-from jarvis.analysis.magnetism.magmom_setup import get_unique_magnetic_structures, tc_mean_field, get_mag_ions
+from jarvis.analysis.magnetism.magmom_setup import MagneticOrdering
+
+
 def test_magnetism_setup():
     from jarvis.db.figshare import get_jid_data
-    from jarvis.core.atoms import get_supercell_dims
+
     atoms = Atoms.from_dict(
         get_jid_data(jid="JVASP-78681", dataset="dft_3d")["atoms"]
     )
-    dim = get_supercell_dims(atoms)
-    print("dim=", dim)
-    dim = [2, 2, 2]
-    symm_list, ss = get_unique_magnetic_structures(
-        atoms, supercell_dim=dim, magnetic_ions=["Mn"]
-    )
-    
-    print("dim=", dim, len(symm_list))
-    assert len(symm_list)==6
-    assert ss.num_atoms == 16
-    mag_atoms=get_mag_ions(atoms)
-    assert mag_atoms==['Mn']
-    tc = tc_mean_field(atoms=atoms)
-    assert round(tc['Tc'],2)==round(7736.345414497057,2) 
+    mag = MagneticOrdering(atoms)
+    symm_list, ss = mag.get_minimum_configs(min_configs=3)
+
+    assert len(symm_list) == 3
+    assert ss.num_atoms == 8
+    mag_atoms = mag.get_mag_ions()
+    assert mag_atoms == ["Mn"]
+    tc = mag.tc_mean_field()
+    assert round(tc["Tc"], 2) == round(7736.345414497057, 2)
