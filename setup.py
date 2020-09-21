5564edf0471c1c185594fe77839f5abcea32c872
Kamal Choudhary <writetokamal.1989@gmail.com>
2020-09-21 02:47:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(base_dir, "README.rst")) as f:
 
 setup(
     name="jarvis-tools",
-    version="2020.9.3",
+    version="2020.9.20",
     long_description=long_d,
     install_requires=[
         "numpy",
