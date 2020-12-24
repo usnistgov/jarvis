29409c2240bbf494f589091a787659e6956dc64a
Kamal Choudhary <writetokamal.1989@gmail.com>
2020-12-24 17:35:37 -0500

    Increase coverage. (#137)
    * Added OQMD, MP, QM9 CFID datasets.
    
    * Added OQMD, MP, QM9 CFID datasets.
    
    * vasp2xml schema update.
    
    * Update config.yml
    
    * XRD DB making bug fix, scattering.json update.
    
    * Minor bug fix in vasp_to_xml.py
    
    * Add license.
    
    * Add license, pin versions
    
    * Remove circle.
    
    * Update shell.nix
    
    * Vasprun single element bug fix.
    
    * Minor updates.
    
    * DB XML and VASP Workflow fix.
    
    * AFLOW CFID added, VASP_PSP_DIR fix.
    
    * Linting fix, new version release.
    
    * Wannier update.
    
    * Version update.
    
    * VASP task update.
    
    * Tempfile for downloading zipfiles.
    
    * Pypi fix.
    
    * Pypi fix.
    
    * Updated wannier, vasp2xml, increased tests.
    
    * Update setup.py
    
    * Update action_build.yml
    
    * Speed up test.
    
    * Update DatasetSummary.rst
    
    * Update outputs.py
    
    * Enforce nk1==nk2 in spillage.
    
    * Update test_spillage.py
    
    * Multiple updates.
    
    * Magtest update.
    
    * Version update.

--- jarvis/tests/testfiles/io/wanniertools/test_outputs.py
+++ jarvis/tests/testfiles/io/wanniertools/test_outputs.py
@@ -1,12 +1,14 @@
 import os
-from jarvis.io.wanniertools.outputs import WTOut
+from jarvis.io.wanniertools.outputs import WTOut, parse_nodes_dat
 
 wt_out = os.path.join(os.path.dirname(__file__), "WT.out")
+wt_nodes = os.path.join(os.path.dirname(__file__), "Nodes.dat")
 
 
 def test_output():
     z2 = WTOut(path=wt_out).get_z2_index()
     chrn = WTOut(path=wt_out).get_chern_number()
+    parse_nodes_dat(wt_nodes)
     assert (z2, chrn[0]) == ("1;0,0,0", 0.0)
 
 
