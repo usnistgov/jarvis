e13b8978794897dd0cc5203aa7499e727af6b1e8
Kamal Choudhary <writetokamal.1989@gmail.com>
2021-01-02 15:32:02 -0500

    Wannier TB for phonons (#138)
    * Added OQMD, MP, QM9 CFID datasets.
    
    * Added OQMD, MP, QM9 CFID datasets.
    
    * vasp2xml schema update.
    
    * Update config.yml
    
    * XRD DB making bug fix, scattering.json update.
    
    * Minor bug fix in vasp_to_xml.py
    
    * Add license.
    
    * Add license, pin versions
    
    * Remove circle.
    
    * Update shell.nix
    
    * Vasprun single element bug fix.
    
    * Minor updates.
    
    * DB XML and VASP Workflow fix.
    
    * AFLOW CFID added, VASP_PSP_DIR fix.
    
    * Linting fix, new version release.
    
    * Wannier update.
    
    * Version update.
    
    * VASP task update.
    
    * Tempfile for downloading zipfiles.
    
    * Pypi fix.
    
    * Pypi fix.
    
    * Updated wannier, vasp2xml, increased tests.
    
    * Update setup.py
    
    * Update action_build.yml
    
    * Speed up test.
    
    * Update DatasetSummary.rst
    
    * Update outputs.py
    
    * Enforce nk1==nk2 in spillage.
    
    * Update test_spillage.py
    
    * Multiple updates.
    
    * Magtest update.
    
    * Version update.
    
    * Update README.rst
    
    * Update README.rst
    
    * WannierTB Phonons.
    
    * phonopy TB.
    
    * phonopy TB.
    
    * Versionupdate.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(base_dir, "README.rst")) as f:
 setup(
     name="jarvis-tools",
 
-    version="2020.12.24",
+    version="2020.12.31",
 
     long_description=long_d,
     install_requires=[
