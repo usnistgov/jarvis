a3ff6900f63f44afb3cdd56845d6da2fa1492d9e
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-02-02 11:50:15 -0500

    Wien2K extra test, ene file.

---
+++ jarvis/tests/testfiles/io/qiskit/test_inputs.py
@@ -0,0 +1,46 @@
+#
+from jarvis.db.figshare import (
+    get_wann_phonon,
+    get_hk_tb,
+    get_wann_electron,
+)
+from jarvis.core.atoms import Atoms
+from jarvis.db.jsonutils import dumpjson
+from jarvis.io.qiskit.inputs import HermitianSolver,get_bandstruct,get_dos
+
+def test_inp():
+    w, ef, atoms = get_wann_electron("JVASP-816")
+    info = get_bandstruct(
+        w=w,
+        line_density=5,
+        atoms=atoms,
+        ef=ef,
+        filename="Alelect.png",
+        ylabel="Energy (eV)",
+    )
+    #dumpjson(data=info, filename="Alelect.json")
+    w, atoms = get_wann_phonon("JVASP-816", factor=34.3)
+    info = get_bandstruct(
+        w=w,
+        line_density=11,
+        atoms=atoms,
+        tol=0.1,
+        filename="Alphon.png",
+        ylabel="Freq.(cm$^{-1}$)",
+    )
+    # dumpjson(data=info,filename='Alphon.json')
+    hk = get_hk_tb(w=w, k=[0.0, 0.0, 0.0])
+    H = HermitianSolver(hk)
+    en, vqe_result, vqe = H.run_vqe(mode="max_val")
+    print("en=", en)
+    eigs,vecs=H.run_vqd()
+    # print(eigs)
+    # print(vecs)
+
+    eigs, vecs = H.run_numpy()
+    # print(eigs)
+    # print(vecs)
+    # get_bandstruct(w=w, atoms=atoms, tol=0.1)
+    get_dos(w=w)
+    H.run_qpe()
+
