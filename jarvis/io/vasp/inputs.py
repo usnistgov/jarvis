9e2324a1e3d73943e733d69a065bb750870c81fe
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-02-02 09:52:24 -0500

    Atoms POSCAR Order update.

--- jarvis/io/vasp/inputs.py
+++ jarvis/io/vasp/inputs.py
@@ -73,9 +73,10 @@ class Poscar(object):
         )
         order = np.argsort(self.atoms.elements)
         coords = self.atoms.frac_coords
-        coords_ordered = np.array(coords)[order]
-        elements_ordered = np.array(self.atoms.elements)[order]
-        props_ordered = np.array(self.atoms.props)[order]
+        # DO NOT USE ORDER
+        coords_ordered = np.array(coords)  # [order]
+        elements_ordered = np.array(self.atoms.elements)  # [order]
+        props_ordered = np.array(self.atoms.props)  # [order]
         # check_selective_dynamics = False
         counts = get_counts(elements_ordered)
         if "T" in "".join(map(str, self.atoms.props[0])):
