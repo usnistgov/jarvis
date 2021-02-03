76265144138e73ea6072fa6b22d6a33bac151ee5
Kamal Choudhary <writetokamal.1989@gmail.com>
2021-02-03 11:31:27 -0500

    Update inputs.py

--- jarvis/io/qiskit/inputs.py
+++ jarvis/io/qiskit/inputs.py
@@ -9,11 +9,9 @@ from qiskit import aqua
 # )
 from jarvis.core.kpoints import generate_kgrid
 from qiskit import Aer
-from qiskit import circuit
-
-# from qiskit.circuit.library import EfficientSU2
+# from qiskit import circuit
+from qiskit.circuit.library import EfficientSU2
 import matplotlib.pyplot as plt
-
 # from qiskit.aqua.algorithms import QPE
 # from qiskit.aqua import QuantumInstance
 # from qiskit.aqua.algorithms import VQE
@@ -67,8 +65,8 @@ class HermitianSolver(object):
             Hamil_mat
         )
         if var_form is None:
-            var_form = circuit.library.EfficientSU2(N, reps=reps)
-            # var_form = EfficientSU2(N, reps=reps)
+            # var_form = circuit.library.EfficientSU2(N, reps=reps)
+            var_form = EfficientSU2(N, reps=reps)
         if optimizer is None:
             vqe = aqua.algorithms.VQE(Hamil_qop, var_form)
             # vqe = VQE(Hamil_qop, var_form)
