1711ccc95683d0f7039c981e4f14067068332f99
Kamal Choudhary <writetokamal.1989@gmail.com>
2021-02-03 14:27:42 -0500

    Update test_hermsolver.py

--- jarvis/tests/testfiles/io/qiskit/test_hermsolver.py
+++ jarvis/tests/testfiles/io/qiskit/test_hermsolver.py
@@ -1,6 +1,7 @@
 #
 #from qiskit.circuit.library import EfficientSU2
 from qiskit.circuit import QuantumCircuit
+from qiskit.circuit import library
 from qiskit import circuit
 from jarvis.db.figshare import (
     get_wann_phonon,
@@ -26,9 +27,9 @@ def test_inp():
     #dumpjson(data=info, filename="Alelect.json")
     w, atoms = get_wann_phonon("JVASP-816", factor=34.3)
     hk = get_hk_tb(w=w, k=[0.0, 0.0, 0.0])
-    var_form = QuantumCircuit(3)
+    var_form = QuantumCircuit(2)
     H = HermitianSolver(hk)
-    en, vqe_result, vqe = H.run_vqe(mode="max_val", var_form=var_form)
+    en, vqe_result, vqe = H.run_vqe(mode="max_val", var_form=var_form,optimizer=None)
     print("en=", en)
     #info = get_bandstruct(
     #    w=w,
