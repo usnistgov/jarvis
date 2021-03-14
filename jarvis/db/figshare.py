675cade089b38ab4f36f33e91ddb38a3aac3decb
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-03-13 21:13:40 -0500

    Try to increase coverage.

--- jarvis/db/figshare.py
+++ jarvis/db/figshare.py
@@ -19,99 +19,104 @@ from tqdm import tqdm
 import matplotlib.image as mpimg
 
 
-def datasets(dataset=""):
-    """Get collection of dataset names and URLs."""
-    if dataset == "dft_2d":
-        # Ref: https://www.nature.com/articles/s41524-020-00440-1
-        url = "https://ndownloader.figshare.com/files/26808917"
-        js_tag = "d2-3-12-2021.json"
-        # url = "https://ndownloader.figshare.com/files/22471019"
-        # js_tag = "jdft_2d-4-26-2020.json"
-        print("Obtaining 2D dataset 1.1k ...")
-    elif dataset == "dft_3d":
-        # Ref: https://www.nature.com/articles/s41524-020-00440-1
-        url = "https://ndownloader.figshare.com/files/26808914"
-        js_tag = "d3-3-12-2021.json"
-        # url = "https://ndownloader.figshare.com/files/22471022"
-        # js_tag = "jdft_3d-4-26-2020.json"
-        print("Obtaining 3D dataset 43k ...")
+def get_db_info():
+    db_info = {
+        "dft_2d": [
+            "https://ndownloader.figshare.com/files/26808917",
+            "d2-3-12-2021.json",
+            "Obtaining 2D dataset 1.1k ...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+        "dft_3d": [
+            "https://ndownloader.figshare.com/files/26808914",
+            "d3-3-12-2021.json",
+            "Obtaining 3D dataset 43k ...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+        "cfid_3d": [
+            "https://ndownloader.figshare.com/files/26808914",
+            "d3-3-12-2021.json",
+            "Obtaining 3D dataset 43k ...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+        "jff": [
+            "https://ndownloader.figshare.com/files/26809760",
+            "jff-3-12-2021.json",
+            "Obtaining JARVIS-FF 2k ...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+        "mp_3d_2020": [
+            "https://ndownloader.figshare.com/files/26791259",
+            "all_mp.json",
+            "Obtaining Materials Project-3D CFID dataset 127k...",
+            "https://doi.org/10.1063/1.4812323",
+        ],
+        "mp_3d": [
+            "https://ndownloader.figshare.com/files/24979850",
+            "CFID_mp_desc_data_84k.json",
+            "Obtaining Materials Project-3D CFID dataset 84k...",
+            "https://doi.org/10.1063/1.4812323",
+        ],
+        "oqmd_3d": [
+            "https://ndownloader.figshare.com/files/24981170",
+            "CFID_OQMD_460k.json",
+            "Obtaining OQMD-3D CFID dataset 460k...",
+            "https://www.nature.com/articles/npjcompumats201510",
+        ],
+        "oqmd_3d_no_cfid": [
+            "https://ndownloader.figshare.com/files/26790182",
+            "all_oqmd.json",
+            "Obtaining OQMD-3D  dataset 800k...",
+            "https://www.nature.com/articles/npjcompumats201510",
+        ],
+        "twod_matpd": [
+            "https://ndownloader.figshare.com/files/26789006",
+            "twodmatpd.json",
+            "Obtaining 2DMatPedia dataset 6k...",
+            "https://www.nature.com/articles/s41597-019-0097-3",
+        ],
+        "qm9": [
+            "https://ndownloader.figshare.com/files/25159592",
+            "qm9_data_cfid.json",
+            "Obtaining 2DMatPedia dataset 6k...",
+            "https://www.nature.com/articles/sdata201422",
+        ],
+        "aflow1": [
+            "https://ndownloader.figshare.com/files/25453256",
+            "CFID_AFLOW1.json",
+            "Obtaining AFLOW-1 CFID dataset 400k...",
+            "https://doi.org/10.1016/j.commatsci.2012.02.005",
+        ],
+        "aflow2": [
+            "https://ndownloader.figshare.com/files/25453265",
+            "CFID_AFLOW2.json",
+            "Obtaining AFLOW-2 CFID dataset 400k...",
+            "https://doi.org/10.1016/j.commatsci.2012.02.005",
+        ],
+        "arXiv": [
+            "https://ndownloader.figshare.com/files/26804795",
+            "arXivdataset.json",
+            "Obtaining arXiv dataset 1.8 million...",
+            "https://www.kaggle.com/Cornell-University/arxiv",
+        ],
+        "cord19": [
+            "https://ndownloader.figshare.com/files/26804798",
+            "cord19.json",
+            "Obtaining CORD19 dataset 223k...",
+            "https://github.com/usnistgov/cord19-cdcs-nist",
+        ],
+        "raw_files": [
+            "https://ndownloader.figshare.com/files/25295732",
+            "figshare_data-10-28-2020.json",
+            "Obtaining raw io files 145k...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+    }
+    return db_info
 
-    elif dataset == "cfid_3d":
-        # Ref: https://doi.org/10.1103/PhysRevMaterials.2.083801
-        # Now same as dft_3d
-        url = "https://ndownloader.figshare.com/files/26808914"
-        js_tag = "d3-3-12-2021.json"
-        # url = "https://ndownloader.figshare.com/files/22470818"
-        # js_tag = "jml_3d-4-26-2020.json"
-        print("Obtaining JARVIS-3D CFID dataset 43k...")
-    elif dataset == "jff":
-        # Ref: 10.1088/1361-648X/aadaff/meta
-        url = "https://ndownloader.figshare.com/files/26809760"
-        js_tag = "jff-3-12-2021.json"
-        print("Obtaining JARVIS-FF 2k ...")
-    elif dataset == "mp_3d":
-        # Ref: https://doi.org/10.1063/1.4812323
-        url = "https://ndownloader.figshare.com/files/24979850"
-        js_tag = "CFID_mp_desc_data_84k.json"
-        print("Obtaining Materials Project-3D CFID dataset 84k...")
-    elif dataset == "mp_3d_2020":
-        # Ref: https://doi.org/10.1063/1.4812323
-        url = "https://ndownloader.figshare.com/files/26791259"
-        js_tag = "all_mp.json"
-        print("Obtaining Materials Project-3D CFID dataset 127k...")
-    elif dataset == "megnet":
-        # Ref: https://doi.org/10.1021/acs.chemmater.9b01294
-        url = "https://ndownloader.figshare.com/files/26724977"
-        js_tag = "megnet.json"
-        print("Obtaining MEGNET-3D CFID dataset 69k...")
-    elif dataset == "oqmd_3d":
-        # Ref: https://www.nature.com/articles/npjcompumats201510
-        url = "https://ndownloader.figshare.com/files/24981170"
-        js_tag = "CFID_OQMD_460k.json"
-        print("Obtaining OQMD-3D CFID dataset 460k...")
-    elif dataset == "oqmd_3d_no_cfid":
-        # Ref: https://www.nature.com/articles/npjcompumats201510
-        url = "https://ndownloader.figshare.com/files/26790182"
-        js_tag = "all_oqmd.json"
-        print("Obtaining OQMD-3D  dataset 800k...")
-    elif dataset == "twod_matpd":
-        # Ref: https://www.nature.com/articles/s41597-019-0097-3
-        url = "https://ndownloader.figshare.com/files/26789006"
-        js_tag = "twodmatpd.json"
-        print("Obtaining 2DMatPedia dataset 6k...")
-    elif dataset == "qm9":
-        # Ref: https://www.nature.com/articles/sdata201422
-        url = "https://ndownloader.figshare.com/files/25159592"
-        js_tag = "qm9_data_cfid.json"
-        print("Obtaining QM9-molecule CFID dataset 134k...")
-    elif dataset == "aflow1":
-        # Ref: https://doi.org/10.1016/j.commatsci.2012.02.005
-        url = "https://ndownloader.figshare.com/files/25453256"
-        js_tag = "CFID_AFLOW1.json"
-        print("Obtaining AFLOW-1 CFID dataset 400k...")
-    elif dataset == "aflow2":
-        # Ref: https://doi.org/10.1016/j.commatsci.2012.02.005
-        url = "https://ndownloader.figshare.com/files/25453265"
-        js_tag = "CFID_AFLOW2.json"
-        print("Obtaining AFLOW-2 CFID dataset 400k...")
-    elif dataset == "arXiv":
-        # Ref: https://www.kaggle.com/Cornell-University/arxiv
-        url = "https://ndownloader.figshare.com/files/26804795"
-        js_tag = "arXivdataset.json"
-        print("Obtaining arXiv dataset 1.8 million...")
-    elif dataset == "cord19":
-        # Ref:https://github.com/usnistgov/cord19-cdcs-nist
-        url = "https://ndownloader.figshare.com/files/26804798"
-        js_tag = "cord19.json"
-        print("Obtaining CORD19 dataset 223k...")
-    elif dataset == "raw_files":
-        # Ref: https://www.nature.com/articles/s41524-020-00440-1
-        url = "https://ndownloader.figshare.com/files/25295732"
-        js_tag = "figshare_data-10-28-2020.json"
-        print("Obtaining raw io files...")
-    else:
-        ValueError("Dataset doesnt exist", dataset)
-    return url, js_tag
+
+# Format: download_link, filename, message, reference
+# Figshare link: https://figshare.com/account/home#/projects/100325
 
 
 def get_stm_2d_dataset():
@@ -186,8 +191,15 @@ def get_request_data(
 
 def data(dataset="dft_2d"):
     """Provide main function to download datasets."""
-    url, js_tag = datasets(dataset)
-    dat = []
+    db_info = get_db_info()
+    if dataset not in list(db_info.keys()):
+        raise ValueError("Check DB name options.")
+    url = db_info[dataset][0]
+    js_tag = db_info[dataset][1]
+    message = db_info[dataset][2]
+    print(message)
+    message = "Reference:" + db_info[dataset][3]
+    print(message)
     # r = requests.get(url)
     # z = zipfile.ZipFile(io.BytesIO(r.content))
     # data = json.loads(z.read(js_tag).decode("utf-8"))
@@ -237,14 +249,11 @@ def get_ff_eneleast():
     return data_ff1
 
 
-# Raw I/O files on figshare repository
-fls = data("raw_files")
-
-
 def get_wann_electron(jid="JVASP-816"):
     """Download electron WTBH if available."""
     w = ""
     ef = ""
+    fls = data("raw_files")
     for i in fls["WANN"]:
         if i["name"].split(".zip")[0] == jid:
             r = requests.get(i["download_url"])
@@ -273,6 +282,8 @@ def get_wann_phonon(jid="JVASP-1002", factor=15.633302):
     """Download phonon WTBH if available."""
     # Requires phonopy
     from jarvis.io.phonopy.outputs import get_phonon_tb
+
+    fls = data("raw_files")
 
     for i in fls["FD-ELAST"]:
         if isinstance(i, dict):
