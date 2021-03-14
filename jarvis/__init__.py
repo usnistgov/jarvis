8709bf57d53387ac1cec976f150ce4f2f75a8f7f
knc6 <writetokamal.1989@gmail.com>
2021-03-14 13:11:49 -0400

    Graph update.

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,2 +1,2 @@
 """Version number."""
-__version__ = "2021.3.11"
+__version__ = "2021.3.12"
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(base_dir, "README.rst")) as f:
 
 setup(
     name="jarvis-tools",
-    version="2021.3.11",
+    version="2021.3.12",
     long_description=long_d,
     install_requires=[
         "numpy>=1.18.5",
