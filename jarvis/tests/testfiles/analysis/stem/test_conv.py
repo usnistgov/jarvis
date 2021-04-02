fcdfb0b9d86e5e611393f5cf44f744ea29037aa8
knc6 <writetokamal.1989@gmail.com>
2021-04-01 21:58:45 -0400

    STEM Conv update.

--- jarvis/tests/testfiles/analysis/stem/test_conv.py
+++ jarvis/tests/testfiles/analysis/stem/test_conv.py
@@ -1,25 +1,20 @@
-from jarvis.core.atoms import crop_squre
 from jarvis.db.figshare import data, get_jid_data
 import matplotlib.pyplot as plt
-from jarvis.core.atoms import Atoms, ase_to_atoms, get_supercell_dims
+from jarvis.core.atoms import Atoms, crop_square
 
 plt.switch_backend("agg")
-from jarvis.analysis.stem.convolution_apprx import (
-    simulate_surface,
-    probe_profile,
-)
+from jarvis.analysis.stem.convolution_apprx import STEMConv
 
 
 def test_conv_method():
+
+    plt.switch_backend("agg")
+
     a = Atoms.from_dict(get_jid_data("JVASP-667")["atoms"])
-    c = crop_squre(a)
-    p = simulate_surface(
-        c,
-        probe_profile=probe_profile(
-            gaussian_width=1, lorentzian_width=1, intensity_ratio=1, n=100
-        ),
-        shape=[50, 50],
-    )
+    c = crop_square(a)
+    # c = a.make_supercell_matrix([2, 2, 1])
+    p = STEMConv(atoms=c).simulate_surface()
+
     # plt.imshow(p, interpolation="gaussian", cmap="plasma")
     # plt.savefig("stem_example.png")
     # plt.close()
