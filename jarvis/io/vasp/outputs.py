Graph angle, QE modules, VASP outputs , XML file update, digitize features, STEM image, Working with fractional coordinates cif files, reduced formula fix added (#157)
* Vasprun single element bug fix.

* Minor updates.

* DB XML and VASP Workflow fix.

* AFLOW CFID added, VASP_PSP_DIR fix.

* Linting fix, new version release.

* Develop (#134)

* Added OQMD, MP, QM9 CFID datasets.

* Added OQMD, MP, QM9 CFID datasets.

* vasp2xml schema update.

* Update config.yml

* XRD DB making bug fix, scattering.json update.

* Minor bug fix in vasp_to_xml.py

* Add license.

* Add license, pin versions

* Remove circle.

* Update shell.nix

* Vasprun single element bug fix.

* Minor updates.

* DB XML and VASP Workflow fix.

* AFLOW CFID added, VASP_PSP_DIR fix.

* Linting fix, new version release.

* Update README.rst

* Wannier update.

* Version update.

* VASP task update.

* Wannier update (#136)

* Added OQMD, MP, QM9 CFID datasets.

* Added OQMD, MP, QM9 CFID datasets.

* vasp2xml schema update.

* Update config.yml

* XRD DB making bug fix, scattering.json update.

* Minor bug fix in vasp_to_xml.py

* Add license.

* Add license, pin versions

* Remove circle.

* Update shell.nix

* Vasprun single element bug fix.

* Minor updates.

* DB XML and VASP Workflow fix.

* AFLOW CFID added, VASP_PSP_DIR fix.

* Linting fix, new version release.

* Wannier update.

* Version update.

* VASP task update.

* Tempfile for downloading zipfiles.

* Pypi fix.

* Pypi fix.

* Updated wannier, vasp2xml, increased tests.

* Update setup.py

* Update action_build.yml

* Speed up test.

* Update DatasetSummary.rst

* Update outputs.py

* Enforce nk1==nk2 in spillage.

* Update test_spillage.py

* Multiple updates.

* Magtest update.

* Version update.

* Increase coverage. (#137)

* Added OQMD, MP, QM9 CFID datasets.

* Added OQMD, MP, QM9 CFID datasets.

* vasp2xm

jarvis/io/vasp/outputs.py
@@ -1044,9 +1044,12 @@ class Wavecar(object):
                 % (Gvec.shape[0], self._nplws[ikpt - 1], np.prod(self._ngrid))
             )
         else:
-            assert Gvec.shape[0] == self._nplws[ikpt - 1], (
-                "No. of planewaves not consistent! %d %d %d"
-                % (Gvec.shape[0], self._nplws[ikpt - 1], np.prod(self._ngrid))
+            assert (
+                Gvec.shape[0] == self._nplws[ikpt - 1]
+            ), "No. of planewaves not consistent! %d %d %d" % (
+                Gvec.shape[0],
+                self._nplws[ikpt - 1],
+                np.prod(self._ngrid),
             )
         self._gvec = np.asarray(Gvec, dtype=int)
 
@@ -1264,6 +1267,26 @@ class Vasprun(object):
         info["masses"] = masses
         info["force_constants"] = force_constants
         return info
+
+    @property
+    def converged_electronic(self):
+        """Check whether electronically converged."""
+        return len(self.ionic_steps[-1]["scstep"]) <= int(
+            self.all_input_parameters["NELM"]
+        )
+
+    @property
+    def converged_ionic(self):
+        """Check whether ionically converged."""
+        nsw = int(self.all_input_parameters["NSW"])
+        if nsw == 0:
+            nsw = 1
+        return len(self.ionic_steps) <= nsw
+
+    @property
+    def converged(self):
+        """Check whether both electronically and ionically converged."""
+        return self.converged_electronic and self.converged_ionic
 
     @property
     def dfpt_data(self, fc_mass=True):