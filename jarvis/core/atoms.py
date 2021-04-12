Graph angle, QE modules, VASP outputs , XML file update, digitize features, STEM image, Working with fractional coordinates cif files, reduced formula fix added (#157)
* Vasprun single element bug fix.

* Minor updates.

* DB XML and VASP Workflow fix.

* AFLOW CFID added, VASP_PSP_DIR fix.

* Linting fix, new version release.

* Develop (#134)

* Added OQMD, MP, QM9 CFID datasets.

* Added OQMD, MP, QM9 CFID datasets.

* vasp2xml schema update.

* Update config.yml

* XRD DB making bug fix, scattering.json update.

* Minor bug fix in vasp_to_xml.py

* Add license.

* Add license, pin versions

* Remove circle.

* Update shell.nix

* Vasprun single element bug fix.

* Minor updates.

* DB XML and VASP Workflow fix.

* AFLOW CFID added, VASP_PSP_DIR fix.

* Linting fix, new version release.

* Update README.rst

* Wannier update.

* Version update.

* VASP task update.

* Wannier update (#136)

* Added OQMD, MP, QM9 CFID datasets.

* Added OQMD, MP, QM9 CFID datasets.

* vasp2xml schema update.

* Update config.yml

* XRD DB making bug fix, scattering.json update.

* Minor bug fix in vasp_to_xml.py

* Add license.

* Add license, pin versions

* Remove circle.

* Update shell.nix

* Vasprun single element bug fix.

* Minor updates.

* DB XML and VASP Workflow fix.

* AFLOW CFID added, VASP_PSP_DIR fix.

* Linting fix, new version release.

* Wannier update.

* Version update.

* VASP task update.

* Tempfile for downloading zipfiles.

* Pypi fix.

* Pypi fix.

* Updated wannier, vasp2xml, increased tests.

* Update setup.py

* Update action_build.yml

* Speed up test.

* Update DatasetSummary.rst

* Update outputs.py

* Enforce nk1==nk2 in spillage.

* Update test_spillage.py

* Multiple updates.

* Magtest update.

* Version update.

* Increase coverage. (#137)

* Added OQMD, MP, QM9 CFID datasets.

* Added OQMD, MP, QM9 CFID datasets.

* vasp2xm

jarvis/core/atoms.py
@@ -11,7 +11,9 @@ from jarvis.core.utils import (
     check_duplicate_coords,
     get_new_coord_for_xyz_sym,
 )
+import os
 import math
+import tempfile
 
 amu_gm = 1.66054e-24
 ang_cm = 1e-8
@@ -175,7 +177,12 @@ class Atoms(object):
         f.close()
 
     @staticmethod
-    def from_cif(filename="atoms.cif", from_string=""):
+    def from_cif(
+        filename="atoms.cif",
+        from_string="",
+        get_primitive_atoms=True,
+        use_cif2cell=True,
+    ):
         """Read .cif format file."""
         # Warnings:
         # May not work for:
@@ -235,7 +242,7 @@ class Atoms(object):
         terminate = False
         count = 0
         while not terminate:
-            print("sym_xyz_line", sym_xyz_line)
+            # print("sym_xyz_line", sym_xyz_line)
             tmp = lines[sym_xyz_line + count + 1]
             if "x" in tmp and "y" in tmp and "z" in tmp:
                 # print("tmp", tmp)
@@ -329,6 +336,70 @@ class Atoms(object):
                         tmp[occupancy_index].split("(")[0]
                     ).is_integer()
                 ):
+                    tmp = " -p vasp --vasp-cartesian-positions --vca -o "
+                    try:
+
+                        if use_cif2cell:
+                            # https://pypi.org/project/cif2cell/
+                            # tested on version 2.0.0a3
+                            try:
+                                new_file, fname = tempfile.mkstemp()
+                                cmd = "cif2cell " + filename + tmp + fname
+                                os.system(cmd)
+                            except Exception as exp:
+                                print(exp)
+                            f = open(fname, "r")
+                            text = f.read().splitlines()
+                            f.close()
+                            elements = (
+                                text[0].split("Species order:")[1].split()
+                            )
+                            scale = float(text[1])
+                            lattice_mat = []
+                            lattice_mat.append(
+                                [float(i) for i in text[2].split()]
+                            )
+                            lattice_mat.append(
+                                [float(i) for i in text[3].split()]
+                            )
+                            lattice_mat.append(
+                                [float(i) for i in text[4].split()]
+                            )
+                            lattice_mat = scale * np.array(lattice_mat)
+                            uniq_elements = elements
+                            element_count = np.array(
+                                [int(i) for i in text[5].split()]
+                            )
+                            elements = []
+                            for i, ii in enumerate(element_count):
+                                for j in range(ii):
+                                    elements.append(uniq_elements[i])
+                            cartesian = True
+                            if "d" in text[6] or "D" in text[6]:
+                                cartesian = False
+                            # print ('cartesian poscar=',cartesian,text[7])
+                            num_atoms = int(np.sum(element_count))
+                            coords = []
+                            for i in range(num_atoms):
+                                coords.append(
+                                    [
+                                        float(i)
+                                        for i in text[7 + i].split()[0:3]
+                                    ]
+                                )
+                            coords = np.array(coords)
+                            atoms = Atoms(
+                                lattice_mat=lattice_mat,
+                                coords=coords,
+                                elements=elements,
+                                cartesian=cartesian,
+                            )
+                            if get_primitive_atoms:
+                                atoms = atoms.get_primitive_atoms
+                            return atoms
+
+                    except Exception as exp:
+                        print(exp)
                     raise ValueError(
                         "Fractional occupancy is not supported.",
                         float(tmp[occupancy_index].split("(")[0]),
@@ -401,6 +472,8 @@ class Atoms(object):
                 cartesian=False,
             )
             cif_atoms = new_atoms
+        if get_primitive_atoms:
+            cif_atoms = cif_atoms.get_primitive_atoms
         return cif_atoms
 
     def write_poscar(self, filename="POSCAR"):
@@ -589,6 +662,127 @@ class Atoms(object):
                     if d[i] > bond_tol:
                         neighbors[i].append([i, j, d[i], image])
         return np.array(neighbors, dtype="object")
+
+    def get_neighbors_cutoffs(self, max_cut=10, r=5, bond_tol=0.15):
+        neighbors = self.get_all_neighbors(r=r, bond_tol=bond_tol)
+        dists = np.hstack(([[xx[2] for xx in yy] for yy in neighbors]))
+        hist, bins = np.histogram(dists, bins=np.arange(0.1, 10.2, 0.1))
+        shell_vol = (
+            4.0
+            / 3.0
+            * np.pi
+            * (np.power(bins[1:], 3) - np.power(bins[:-1], 3))
+        )
+        arr = []
+        for i, j in zip(bins[:-1], hist):
+            if j > 0:
+                arr.append(i)
+        rcut_buffer = 0.11
+        io1 = 0
+        io2 = 1
+        io3 = 2
+        try:
+            delta = arr[io2] - arr[io1]
+            while delta < rcut_buffer and arr[io2] < max_cut:
+                io1 = io1 + 1
+                io2 = io2 + 1
+                io3 = io3 + 1
+                delta = arr[io2] - arr[io1]
+
+            rcut1 = (arr[io2] + arr[io1]) / float(2.0)
+        except Exception:
+            print("Warning:Setting first nbr cut-off as minimum bond-dist")
+            rcut1 = arr[0]
+            pass
+        try:
+            delta = arr[io3] - arr[io2]
+            while (
+                delta < rcut_buffer
+                and arr[io3] < max_cut
+                and arr[io2] < max_cut
+            ):
+                io2 = io2 + 1
+                io3 = io3 + 1
+                delta = arr[io3] - arr[io2]
+            rcut2 = float(arr[io3] + arr[io2]) / float(2.0)
+        except Exception:
+            print("Warning:Setting first and second nbr cut-off equal")
+            print("You might consider increasing max_n parameter")
+            rcut2 = rcut1
+            pass
+        return rcut1, rcut2, neighbors
+
+    def atomwise_angle_and_radial_distribution(
+        self, r=5, bond_tol=0.15, c_size=10, verbose=False
+    ):
+        rcut1, rcut2, neighbors = self.get_neighbors_cutoffs(
+            r=r, bond_tol=bond_tol
+        )
+        from jarvis.analysis.structure.neighbors import NeighborsAnalysis
+        from jarvis.core.utils import bond_angle as angle
+
+        nbor_info = NeighborsAnalysis(
+            self, rcut1=rcut1, rcut2=rcut2
+        ).nbor_list(rcut=rcut1, c_size=c_size)
+        nat = nbor_info["nat"]
+        dist = nbor_info["dist"]
+        atom_rdfs = []
+        nbins = 180
+        actual_prdf = []
+        # actual_pangs = []
+        actual_pangs = np.zeros((nat, 380))
+        for i in range(nat):
+            hist, bins = np.histogram(
+                dist[:, i], bins=np.arange(0.1, rcut1 + 0.2, 0.1)
+            )
+            actual_prdf.append(dist[:, i])
+            atom_rdfs.append(hist.tolist())
+            if verbose:
+                exact_dists = np.arange(0.1, rcut1 + 0.2, 0.1)[hist.nonzero()]
+                print("exact_dists", exact_dists)
+        # prdf_arr = np.array(atom_rdfs)[0 : self.num_atoms]
+
+        atom_angles = []
+        for i in range(nbor_info["nat"]):
+            angles = [
+                angle(
+                    nbor_info["dist"][in1][i],
+                    nbor_info["dist"][in2][i],
+                    nbor_info["bondx"][in1][i],
+                    nbor_info["bondx"][in2][i],
+                    nbor_info["bondy"][in1][i],
+                    nbor_info["bondy"][in2][i],
+                    nbor_info["bondz"][in1][i],
+                    nbor_info["bondz"][in2][i],
+                )
+                for in1 in range(nbor_info["nn"][i])
+                for in2 in range(nbor_info["nn"][i])
+                if in2 > in1
+                and nbor_info["dist"][in1][i] * nbor_info["dist"][in2][i] != 0
+            ]
+            ang_hist, ang_bins = np.histogram(
+                angles,
+                bins=np.arange(1, nbins + 2, 1),
+                density=False,
+            )
+            for jj, j in enumerate(angles):
+                actual_pangs[i, jj] = j
+            # actual_pangs.append(angles)
+            atom_angles.append(ang_hist)
+            if verbose:
+                exact_angles = np.arange(1, nbins + 2, 1)[ang_hist.nonzero()]
+                print("exact_angles", exact_angles)
+        # return (atom_angles)#/nbor_info['nat']
+
+        pangle_arr = np.array(atom_angles)[0 : self.num_atoms]
+        return (
+            neighbors,
+            np.array(atom_rdfs)[0 : self.num_atoms],
+            np.array(atom_angles)[0 : self.num_atoms],
+            np.array(actual_prdf[0 : self.num_atoms]),
+            np.array(actual_pangs[0 : self.num_atoms]),
+            nbor_info,
+        )
 
     @property
     def raw_distance_matrix(self):
@@ -1355,6 +1549,39 @@ def ase_to_atoms(ase_atoms=""):
         coords=ase_atoms.get_positions(),
         pbc=True,
     )
+
+
+def crop_square(atoms=None, csize=10):
+    """Crop a sqaur portion from a surface/2D system."""
+    sz = csize / 2
+    # just to make sure we have enough material to crop from
+    enforce_c_size = sz * 3
+    dims = get_supercell_dims(atoms, enforce_c_size=enforce_c_size)
+    b = atoms.make_supercell_matrix(dims).center_around_origin()
+    lat_mat = [
+        [enforce_c_size, 0, 0],
+        [0, enforce_c_size, 0],
+        [0, 0, b.lattice_mat[2][2]],
+    ]
+    M = np.linalg.solve(b.lattice_mat, lat_mat)
+    tol = 3
+
+    els = []
+    coords = []
+    for i, j in zip(b.cart_coords, b.elements):
+        if i[0] <= sz and i[0] >= -sz and i[1] <= sz and i[1] >= -sz:
+            els.append(j)
+            coords.append(i)
+    coords = np.array(coords)
+    new_mat = (
+        [max(coords[:, 0]) - min(coords[:, 0]) + tol, 0, 0],
+        [0, max(coords[:, 1]) - min(coords[:, 1]) + tol, 0],
+        [0, 0, b.lattice_mat[2][2]],
+    )
+    new_atoms = Atoms(
+        lattice_mat=lat_mat, elements=els, coords=coords, cartesian=True
+    ).center_around_origin([0.5, 0.5, 0.5])
+    return new_atoms
 
 
 """

jarvis/core/graphs.py
@@ -6,7 +6,6 @@ import numpy as np
 from collections import OrderedDict
 from jarvis.analysis.structure.neighbors import NeighborsAnalysis
 from jarvis.core.specie import get_node_attributes
-import warnings
 from typing import List, Tuple
 from jarvis.core.atoms import Atoms
 from collections import defaultdict
@@ -16,7 +15,7 @@ try:
     from tqdm import tqdm
     import dgl
 except Exception as exp:
-    warnings.warn("dgl/torch/tqdm is not installed.", exp)
+    print("dgl/torch/tqdm is not installed.", exp)
     pass
 
 
@@ -61,13 +60,56 @@ class Graph(object):
         atom_features="cgcnn",
         enforce_undirected=False,
         max_attempts=3,
+        include_prdf_angles=False,
+        partial_rcut=4.0,
         id=None,
     ):
+
         """Obtain a DGLGraph for Atoms object."""
-        all_neighbors = atoms.get_all_neighbors(r=cutoff)
+        dists = atoms.raw_distance_matrix
+
+        def cos_formula(a, b, c):
+            """Get angle between three edges for oblique triangles."""
+            res = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
+            res = -1.0 if res < -1.0 else res
+            res = 1.0 if res > 1.0 else res
+            return np.arccos(res)
+
+        def bond_to_bond_feats(nb):
+            tmp = 0
+            angles_tmp = []
+            for ii, i in enumerate(nb):
+                tmp = ii + 1
+                if tmp > len(nb) - 1:
+                    tmp = 0
+                ang = 0
+                try:
+                    ang = cos_formula(
+                        i[2], nb[tmp][2], dists[i[1], nb[tmp][1]]
+                    )
+                except Exception as exp:
+                    # print("Setting angle zeros", id, exp)
+                    pass
+                angles_tmp.append(ang)
+            return np.array(angles_tmp)
+
+        if include_prdf_angles:
+            (
+                all_neighbors,
+                prdf_arr,
+                pangle_arr,
+                pval,
+                aval,
+                nbor,
+            ) = atoms.atomwise_angle_and_radial_distribution(r=cutoff)
+            pval = np.fliplr(np.sort(pval))[:, 0:max_neighbors]
+            aval = np.fliplr(np.sort(aval))[:, 0:max_neighbors]
+        else:
+            all_neighbors = atoms.get_all_neighbors(r=cutoff)
         # if a site has too few neighbors, increase the cutoff radius
         min_nbrs = min(len(neighborlist) for neighborlist in all_neighbors)
         # print('min_nbrs,max_neighbors=',min_nbrs,max_neighbors)
+
         attempt = 0
         while min_nbrs < max_neighbors:
             print("extending cutoff radius!", attempt, cutoff, id)
@@ -104,7 +146,7 @@ class Graph(object):
         # so what's left is the odd ones out
         edges = defaultdict(list)
 
-        u, v, r = [], [], []
+        u, v, r, w, prdf, adf = [], [], [], [], [], []
         for site_idx, neighborlist in enumerate(all_neighbors):
 
             # sort on distance
@@ -119,13 +161,22 @@ class Graph(object):
 
             # keep all edges out to the neighbor shell of the k-th neighbor
             ids = ids[distances <= max_dist]
+            new_angles = bond_to_bond_feats(neighborlist)
+            try:
+                new_angles = new_angles[ids - 1]
+            except Exception as exp:
+                new_angles = np.zeros(len(ids))
+                pass
             c = c[distances <= max_dist]
             distances = distances[distances <= max_dist]
-
             u.append([site_idx] * len(ids))
             v.append(ids)
             r.append(distances)
+            w.append(new_angles)
 
+            if include_prdf_angles:
+                prdf.append(pval[site_idx])
+                adf.append(aval[site_idx])
             # keep track of cell-resolved edges
             # to enforce undirected graph construction
             for dst, cell_id in zip(ids, c):
@@ -143,22 +194,51 @@ class Graph(object):
                     v.append(src)
                     r.append(atoms.raw_distance_matrix[src, dst])
 
-        u = torch.tensor(np.hstack(u))
-        v = torch.tensor(np.hstack(v))
-        r = torch.tensor(np.hstack(r)).type(torch.get_default_dtype())
+        u = np.hstack(u)
+        v = np.hstack(v)
+        r = np.hstack(r)
+        w = np.hstack(w)
+        u = torch.tensor(u)
+        v = torch.tensor(v)
+        w = torch.tensor(w)
+        if include_prdf_angles:
+            prdf = np.array(prdf)
+            adf = np.array(adf)
+            prdf = np.hstack(prdf)
+            adf = np.cos(np.hstack(adf))
+            if len(r) != len(prdf):
+                prdf = np.append(prdf, np.zeros(len(r) - len(prdf)))
+            if len(r) != len(adf):
+                adf = np.append(adf, np.zeros(len(r) - len(adf)))
+            prdf = torch.tensor(np.array(np.hstack(prdf))).type(
+                torch.get_default_dtype()
+            )
+            adf = torch.tensor(np.array(np.hstack(adf))).type(
+                torch.get_default_dtype()
+            )
 
+        r = torch.tensor(np.array(r)).type(torch.get_default_dtype())
+        w = torch.tensor(np.array(w)).type(torch.get_default_dtype())
         # build up atom attribute tensor
         species = atoms.elements
-        node_features = torch.tensor(
-            [
-                get_node_attributes(s, atom_features=atom_features)
-                for s in species
-            ]
-        ).type(torch.get_default_dtype())
+        sps_features = []
+        for ii, s in enumerate(species):
+            feat = list(get_node_attributes(s, atom_features=atom_features))
+            # if include_prdf_angles:
+            #    feat=feat+list(prdf[ii])+list(adf[ii])
+            sps_features.append(feat)
+        sps_features = np.array(sps_features)
+        node_features = torch.tensor(sps_features).type(
+            torch.get_default_dtype()
+        )
 
         g = dgl.graph((u, v))
         g.ndata["atom_features"] = node_features
         g.edata["bondlength"] = r
+        g.edata["bondangle"] = w
+        if include_prdf_angles:
+            g.edata["partial_distance"] = prdf
+            g.edata["partial_angle"] = adf
 
         return g
 
@@ -389,6 +469,7 @@ class StructureDataset(torch.utils.data.Dataset):
         transform=None,
         enforce_undirected=False,
         max_neighbors=12,
+        classification=False,
     ):
         """Initialize the class."""
         self.graphs = []
@@ -416,6 +497,10 @@ class StructureDataset(torch.utils.data.Dataset):
             self.ids.append(id)
 
         self.labels = torch.tensor(self.labels).type(torch.get_default_dtype())
+        if classification:
+            self.labels = self.labels.view(-1).long()
+            print("Classification dataset.", self.labels)
+
         self.transform = transform
 
     def __len__(self):
@@ -447,7 +532,11 @@ class StructureDataset(torch.utils.data.Dataset):
         """Dataloader helper to batch graphs cross `samples`."""
         graphs, labels = map(list, zip(*samples))
         batched_graph = dgl.batch(graphs)
-        return batched_graph, torch.tensor(labels)
+        device = "cpu"
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+
+        return batched_graph.to(device), torch.tensor(labels).to(device)
 
 
 """

jarvis/core/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import math
 import xmltodict
 import re
+import requests
 
 
 def xml_to_dict(fname):
@@ -237,9 +238,71 @@ def operate_affine(cart_coord=[], affine_matrix=[]):
     return np.dot(np.array(affine_matrix), affine_point)[0:3]
 
 
+def gaussian(x, sigma):
+    """Get Gaussian profile."""
+    return np.exp(-(x ** 2) / (2 * sigma ** 2))
+
+
+def lorentzian2(x, gamma):
+    """Get Lorentziann profile."""
+    return (
+        gamma
+        / 2
+        / (np.pi * (x ** 2 + (gamma / 2) ** 2))
+        / (2 / (np.pi * gamma))
+    )
+
+
+def digitize_array(values=[], max_len=10):
+    """Digitze an array."""
+    has_float = False in [float(i).is_integer() for i in values]
+    if has_float:
+        arr = np.array([float(i) for i in values])
+        max_val = max(arr)
+        min_val = min(arr)
+        bins = np.arange(1, max_len + 1) * (max_val - min_val) / 10
+        values = np.digitize(arr, bins)
+    return values
+
+
+def bond_angle(
+    dist1,
+    dist2,
+    bondx1,
+    bondx2,
+    bondy1,
+    bondy2,
+    bondz1,
+    bondz2,
+):
+    """Get an angle."""
+    nm = dist1 * dist2
+    rrx = bondx1 * bondx2
+    rry = bondy1 * bondy2
+    rrz = bondz1 * bondz2
+    cos = (rrx + rry + rrz) / (nm)
+    if cos <= -1.0:
+        cos = cos + 0.000001
+    if cos >= 1.0:
+        cos = cos - 0.000001
+    deg = math.degrees(math.acos(cos))
+    return deg
+
+
+def check_url_exists(
+    url="https://www.ctcms.nist.gov/~knc6/static/JARVIS-DFT/JVASP-77580.xml",
+):
+    """Check if a url exists."""
+    request = requests.get(url)
+    if request.status_code == 200:
+        return True
+    else:
+        return False
+
+
 # def is_xml_valid(xsd="jarvisdft.xsd", xml="JVASP-1002.xml"):
-#    """Check if XML is valid."""
-#    xml_file = etree.parse(xml)
-#    xml_validator = etree.XMLSchema(file=xsd)
-#    is_valid = xml_validator.validate(xml_file)
-#    return is_valid
+#   """Check if XML is valid."""
+#   xml_file = etree.parse(xml)
+#   xml_validator = etree.XMLSchema(file=xsd)
+#   is_valid = xml_validator.validate(xml_file)
+#   return is_valid