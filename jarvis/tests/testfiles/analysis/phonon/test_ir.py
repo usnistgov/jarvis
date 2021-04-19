c96fc533534d2d00aae58512d103a9019646643c
knc6 <writetokamal.1989@gmail.com>
2021-04-19 13:37:10 -0400

    Fix IR pytest.

--- jarvis/tests/testfiles/analysis/phonon/test_ir.py
+++ jarvis/tests/testfiles/analysis/phonon/test_ir.py
@@ -16,6 +16,13 @@ vrun_file = os.path.join(
     "vasp",
     "vasprun.xml.JVASP-39",
 )
+dirc = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "..",
+    "io",
+    "vasp",
+)
 
 vrun = Vasprun(vrun_file)
 
@@ -35,7 +42,7 @@ def test_ir():
     print(max(y))
     assert round(max(y), 2) == round(0.3511482090386446, 2)
     pdos = vrun.partial_dos_spdf
-    x, y = ir_intensity_phonopy(vasprun=vrun_file)
+    x, y = ir_intensity_phonopy(vasprun=vrun_file, run_dir=dirc)
 
 
 test_ir()
