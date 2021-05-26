5cf19335f5853baac2f70b551c2e76effd02411b
Kamal Choudhary <writetokamal.1989@gmail.com>
2021-05-25 23:42:36 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,10 +17,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.dirname(".."))
-sys.path.insert(0, os.path.dirname("../jarvis"))
-sys.path.insert(0, os.path.dirname("../.."))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.dirname(__file__))
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
