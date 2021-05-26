105c00ecd59f1fc0406581e35f13ac0da4871181
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-05-26 04:51:33 -0400

    Update conf.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,6 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'JARVIS-Tools'
-#project = u'Django Kong'
 copyright = u'2021, Kamal Choudhary'
 
 # The version info for the project you're documenting, acts as replacement for
