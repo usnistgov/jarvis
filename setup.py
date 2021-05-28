a0bcea3b3f84cbac467ffdfad5d00b4fb3ddb68d
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-05-28 00:52:59 -0400

    Add DOS TB test.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(base_dir, "README.rst")) as f:
 
 setup(
     name="jarvis-tools",
-    version="2021.05.25",
+    version="2021.05.27",
     long_description=long_d,
     install_requires=[
         "numpy>=1.19.5",
