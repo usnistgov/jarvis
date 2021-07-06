cc1e27309b84a7f2aa92c3135b744e3c22a5d66b
knc6 <writetokamal.1989@gmail.com>
2021-07-05 20:10:53 -0400

    Version update, COD DB, QM9 JCTC DB added.

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,2 +1,2 @@
 """Version number."""
-__version__ = "2021.06.30"
+__version__ = "2021.07.04"
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(base_dir, "README.rst")) as f:
 
 setup(
     name="jarvis-tools",
-    version="2021.06.30",
+    version="2021.07.04",
     long_description=long_d,
     install_requires=[
         "numpy>=1.19.5",
