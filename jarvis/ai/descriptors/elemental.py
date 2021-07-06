d2dfe562bbcb5b7513c79ac9f306433fa65974cb
Kamal Choudhary <writetokamal.1989@gmail.com>
2021-07-06 02:50:44 -0400

    Develop (#185)
    * Image augmentation.
    
    * Augment images.
    
    * Augment images.
    
    * Specie update.
    
    * Add Latt2D, STM image (b-1) fix, image augmentation fix.
    
    * Add Latt2D, STM image (b-1) fix, image augmentation fix.
    
    * Update conf.py
    
    * Update conf.py
    
    * Multi-output graph bacthing.
    
    * Add EDOS dataset.
    
    * Temp.
    
    * Add circuit maker.
    
    * Add circuit maker.
    
    * NELECT update.
    
    * Version update, more DBs added.
    
    * Fix CHGCAR vasp.
    
    * Added volumetric reshape for CHGCAR.
    
    * Tmp
    
    * Tershoff Hamman update, specie update.
    
    * Add crop from center in STM.
    
    * Add Fourier transfor in STM.
    
    * Update STM pytest.
    
    * Add DPI to STM.
    
    * Zeo++ added, Atoms cif update, STM update, random vacancy maker added.
    
    * Atoms tempfile fix, Potcar from atoms module added.
    
    * Test for docs.
    
    * C2DB link update, docs Atoms update.
    
    * C2DB link update, docs Atoms update.
    
    * Version update, COD DB, QM9 JCTC DB added.
    
    * Compostion bug fix, elemental descriptor added.

---
+++ jarvis/ai/descriptors/elemental.py
@@ -0,0 +1,14 @@
+"""Module to get elemnt fraction based array for a formula."""
+# Maximum upto 103 elements
+import numpy as np
+from jarvis.core.composition import Composition
+from jarvis.core.specie import Specie
+
+
+def get_element_fraction_desc(formula="SiO2", max_nelements=103):
+    x = np.zeros(max_nelements)
+    fracs = Composition.from_string(formula).atomic_fraction
+    for i, j in fracs.items():
+        # -1 because python array srats from 0
+        x[Specie(i).Z - 1] = j
+    return x
