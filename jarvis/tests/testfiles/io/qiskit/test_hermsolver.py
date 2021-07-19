78f56dc1db0caea216c9bbac0598126b21e83b8c
knc6 <writetokamal.1989@gmail.com>
2021-07-19 08:39:00 -0400

    Reduced pytest.

--- jarvis/tests/testfiles/io/qiskit/test_hermsolver.py
+++ jarvis/tests/testfiles/io/qiskit/test_hermsolver.py
@@ -19,6 +19,9 @@ import numpy as np
 from qiskit import Aer, execute
 
 import math
+import gc
+
+gc.collect()
 
 def nCr(n,r):
     f = math.factorial
@@ -67,7 +70,8 @@ def test_inp():
     hk = get_hk_tb(w=w, k=[0.0, 0.0, 0.0])
     print ('shape=',hk.shape)
     H = HermitianSolver(hk)
-    en, vqe_result, vqe = H.run_vqe(mode="max_val", var_form=qc)#,optimizer=optimizer)
+    en, vqe_result, vqe = H.run_vqe(mode="max_val", reps=1)#,optimizer=optimizer)
+   # en, vqe_result, vqe = H.run_vqe(mode="max_val", var_form=qc)#,optimizer=optimizer)
     print("en=", en)
     info = get_bandstruct(
         w=w,
@@ -88,5 +92,5 @@ def test_inp():
     print(eigs)
     # print(vecs)
     # get_bandstruct(w=w, atoms=atoms, tol=0.1)
-    get_dos(w=w)
+    get_dos(w=w,grid=[1,1,1])
     H.run_qpe()
