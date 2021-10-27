87835eb82077832c36b3c99d60a23e0317f88fb0
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-10-27 10:17:29 -0400

    vacancy update.

--- jarvis/analysis/defects/vacancy.py
+++ jarvis/analysis/defects/vacancy.py
@@ -67,13 +67,13 @@ class Vacancy(object):
             dim2 = int(float(enforce_c_size) / float(b)) + extend
             dim3 = int(float(enforce_c_size) / float(c)) + extend
             # atoms = atoms.make_supercell([dim1, dim2, dim3])
-            supercell_size = [dim1, dim2, dim3]
             if dim1 == 0:
                 dim1 = 1
             if dim2 == 0:
                 dim2 = 1
             if dim3 == 0:
                 dim3 = 1
+            supercell_size = [dim1, dim2, dim3]
         spg = Spacegroup3D(atoms)
         wyckoffs = spg._dataset["wyckoffs"]
         atoms.props = wyckoffs
