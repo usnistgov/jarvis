e7ff33bfbebc2ddd4bca98a2a1d2e588693dca5a
SauravMaheshkar <sauravvmaheshkar@gmail.com>
2021-12-26 14:17:25 +0530

    Add installation tests

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,2 +1,11 @@
 """Version number."""
 __version__ = "2021.12.16"
+
+import os
+
+def test(*args):
+
+    import pytest
+
+    path = os.path.join(os.path.split(__file__)[0], "tests")
+    pytest.main(args = [path] + list(args))
