7805fcdde8b09296493aef03001ba13509ec45de
SauravMaheshkar <sauravvmaheshkar@gmail.com>
2021-12-28 14:05:08 +0530

    Fix codestyle with black

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -3,11 +3,10 @@ __version__ = "2021.12.16"
 
 import os
 
-def test(*args):
 
+def test(*args):
 
     import pytest
 
     path = os.path.join(os.path.split(__file__)[0], "tests")
     pytest.main(args=[path] + list(args))
-
