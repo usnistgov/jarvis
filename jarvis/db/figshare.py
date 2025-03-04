100c1779a13eb65a09efd59a8ea30bbf303aff61
Kamal Choudhary <writetokamal.1989@gmail.com>
2025-03-04 15:59:51 -0500

    Update figshare.py

--- jarvis/db/figshare.py
+++ jarvis/db/figshare.py
@@ -325,7 +325,7 @@ def get_db_info():
         ],
         # https://doi.org/10.6084/m9.figshare.23000573
         "vacancydb": [
-            "https://figshare.com/ndownloader/files/40750811",
+            "https://figshare.com/ndownloader/files/41219922",
             "vacancydb.json",
             "Obtaining vacancy dataset 464...",
             "https://doi.org/10.1063/5.0135382",
